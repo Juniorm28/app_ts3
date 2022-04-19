a4d3a391336a1638e2d759a164d748d449fd79cf
Juniorm28 <martinezlopezjunior28@gmail.com>
2022-04-18 19:00:59 -0500

    Paso 4

--- Backend/src/config/index.ts
+++ Backend/src/config/index.ts
@@ -1,1 +1,22 @@
-console.log("Hola a Todos!!")
+import express, { Application   } from 'express';
+
+ export class App {
+    app: Application;
+
+    constructor(
+       private port?: number | string
+    ) {
+        this.app = express();
+        this.settings();
+    }
+
+
+   private settings(){
+       this.app.set('port', this.port  || process.env.PORT  || 3000);
+   }
+
+   async listen() {
+     await this.app.listen(this.app.get('port'));
+        console.log('Server en el puerto: ', this.app.get('port')); 
+   }
+}
