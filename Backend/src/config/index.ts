d198e344decdc52ecc993f221ecbd3005634db9f
Juniorm28 <martinezlopezjunior28@gmail.com>
2022-04-18 19:14:21 -0500

    Paso 6

--- Backend/src/config/index.ts
+++ Backend/src/config/index.ts
@@ -1,4 +1,5 @@
 import express, { Application   } from 'express';
+import morgan from 'morgan';
 
  export class App {
     app: Application;
@@ -8,11 +9,19 @@ import express, { Application   } from 'express';
     ) {
         this.app = express();
         this.settings();
+        this.middlewares();
+    }
+
+    private middlewares() {
+        this.app.use(morgan('dev'));
+        this.app.use(express.json()); //valores tipo JSON
+        this.app.use(express.urlencoded({ extended: false})); //valores formulario
+
     }
 
 
    private settings(){
-       this.app.set('port', this.port  || process.env.PORT  || 3000);
+    this.app.set('port', this.port || 3000);
    }
 
    async listen() {
