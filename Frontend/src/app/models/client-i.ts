83de21a6dfcb3d4cf705f7c275a4ab53b2873b61
Juniorm28 <martinezlopezjunior28@gmail.com>
2022-04-22 10:44:37 -0500

    Paso 27

--- Frontend/src/app/models/client-i.ts
+++ Frontend/src/app/models/client-i.ts
@@ -1,2 +1,12 @@
 export interface ClientI {
+    id?: number,
+    nombres?: string,
+    apellidos?: string,
+    correo?: string,
+    telefonos?: string,
+    celular?: string,
+    fechaNacimiento?: Date,
+    direccion?: string,
+    createdAt?: string,
+    updatedAt?: string
 }
--- Frontend/src/app/services/clients.service.ts
+++ Frontend/src/app/services/clients.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ClientI } from '../models/client-i';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientsService {
 
-  constructor() { }
+  API_URI = 'http://localhost:5000';
+
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get(`${this.API_URI}/clientes`);
+  }
 }
