d994ec195b3653ba355b25dd825a31e676384f00
Juniorm28 <martinezlopezjunior28@gmail.com>
2022-04-22 10:33:01 -0500

    Paso 24

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormLoginComponent } from './componenets/user/form-login/form-login.component'
+import { FormLoginComponent } from './componenets/user/form-login/form-login.component';
+import { ClientListComponent } from './componenets/client/client-list/client-list.component'
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: FormLoginComponent
+  },
+  {
+    path: 'cliente',
+    component: ClientListComponent
   }
 ];
 
