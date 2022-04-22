436d866f00a2509f0f7de51528093ab234970ae4
Juniorm28 <martinezlopezjunior28@gmail.com>
2022-04-22 10:02:51 -0500

    Paso 19

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './componenets/layouts/main/header/menu/menu.component';
+import { CopyrightComponent } from './componenets/layouts/main/footer/copyright/copyright.component';
+import { FormLoginComponent } from './componenets/user/form-login/form-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    CopyrightComponent,
+    FormLoginComponent
   ],
   imports: [
     BrowserModule,
